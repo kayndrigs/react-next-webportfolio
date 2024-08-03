5a1966e394f55759394e03826afc24d7f1ef8b57
kayne.rodrigo11 <kayne.rodrigo11@gmail.com>
2024-08-03 08:55:34 +0000

    added changes

--- components/About.jsx
+++ components/About.jsx
@@ -49,12 +49,12 @@ const qualificationData = [
         data: [
             {
                 company: 'Synthillate',
-                qualitifcation: 'Digital Marketing Intern',
+                role: 'Digital Marketing Intern',
                 years: '2021-2021'
             },
             {
                 company: 'S.P. Madrid & Associates',
-                qualitifcation: 'Data Science Intern',
+                role: 'Data Science Intern',
                 years: '2024-2024'
             },
 
@@ -92,7 +92,8 @@ const About = () => {
   return (
     <section className='xl:h-[860px] pb-12 xl:py-24'>
         <div className = "container mx-auto">
-            <h2 className='section-title xl:mb-16 text-center mx-auto'>About me</h2>
+            <h2 className='section-title mb-16 xl:mb-16 text-center mx-auto'>About me</h2>
+
             <div className='flex flex-col xl:flex-row'>
                 {/* image */}
                 <div className="hidden xl:flex flex-1 relative">
@@ -101,22 +102,129 @@ const About = () => {
                 {/* tabs */}
                 <div className='flex-1'>
                     <Tabs defaultValue='personal'>
-                        <TabsList>
-                            <TabsTrigger value="personal">Personal Info</TabsTrigger>
-                            <TabsTrigger value="qualifications">Qualifications</TabsTrigger>
-                            <TabsTrigger value="skills">Skills</TabsTrigger>
+      
+                    <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                            <TabsTrigger  value="personal">Personal Info</TabsTrigger>
+                            <TabsTrigger  value="qualifications">Qualifications</TabsTrigger>
+                            <TabsTrigger  value="skills">Skills</TabsTrigger>
                         </TabsList>
                         {/* tabs content*/}
-                        <div>
+                        <div className='text-lg mt-12 xl:mt-8'>
                             {/* personal */}
                             <TabsContent value="personal">
-                                personal info
+                                <div className="text-center xl:text-left">
+                                    <h3 className='h3 mb-4'>Unmatched Service Quality for Over 10 Years</h3>
+                                    <p className="subtitle max-w-xl mx-auto xl:ml-0">I specialize in crafting intuitive websites with cutting-edge technology, delivering dynamic  and engaging user experiences.</p>
+                                    {/* icons */}
+                                    <div className='grid xl:grid-cols-2 gap-4 mb-12'>
+                                        {infoData.map((item, index) => {
+                                            return (
+                                            <div className="flex items-center gap-x-4 mx-auto xl:mx-0" key={index}>
+                                                <div className='text-primary'>{item.icon}</div>
+                                                <div>{item.text}</div>
+                                            </div>
+                                            );
+                                        })}
+                  
+                                    </div>
+                                    {/* languages */}
+                                    <div className="flex flex-col gap-y-2">
+                                        <div className="text-primary">
+                                            Language Skill
+                                        </div>
+                                        <div className="border-b border-border"></div>
+                                        <div>English & Filipino</div>
+                                    </div>
+                                </div>
                             </TabsContent>
+                            {/* qualifications */}
+
                             <TabsContent value="qualifications">
-                                qualifications
+                                <div>
+                                    <h3 className='h3 mb-8 text-center xl:text-left'>
+                                        My Awesome Journey
+                                    </h3>
+                                    {/* experience and education wrapper */}
+                                    <div className='grid md:grid-cols-2 gap-y-8'>
+                                        {/* experience */}
+                                        <div className='flex flex-col gap-y-6'>
+                                            <div className='flex gap-x-4 items-center text-[22px] text-primary'>
+                                                
+                                                <Briefcase />
+                                                <h4 className='capitalize font-medium'>
+                                                    {getData(qualificationData, 'experience').title}
+                                                </h4>
+                                            </div>
+                                            {/* list */}
+                                            <div className='flex flex-col gap-y-8'>
+                                                {getData(qualificationData, 'experience').data.map((item, index)=> {
+                                                    const {company, role, years} = item;
+                                                    return (
+                                                        <div className='flex gap-x-8 group' key={index}>
+                                                            <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                            </div>
+                                                            <div>
+                                                                <div className="font-semibold text-xl leading-none mb-2">{company}</div>
+                                                                <div className="text-lg leading-none text-muted-foreground mb-4">{role}</div>
+                                                                <div className="text-base font-medium">{years}</div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                        {/* education */}
+                                        <div className='flex flex-col gap-y-6'>
+                                            <div className='flex gap-x-4 items-center text-[22px] text-primary'>
+                                                
+                                                <GraduationCap size={28}/>
+                                                <h4 className='capitalize font-medium'>
+                                                    {getData(qualificationData, 'education').title}
+                                                </h4>
+                                            </div>
+                                            {/* list */}
+                                            <div className='flex flex-col gap-y-8'>
+                                                {getData(qualificationData, 'education').data.map((item, index)=> {
+                                                    const {university, qualification, years} = item;
+                                                    return (
+                                                        <div className='flex gap-x-8 group' key={index}>
+                                                            <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                                <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                            </div>
+                                                            <div>
+                                                                <div className="font-semibold text-xl leading-none mb-2">{university}</div>
+                                                                <div className="text-lg leading-none text-muted-foreground mb-4">{qualification}</div>
+                                                                <div className="text-base font-medium">{years}</div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </TabsContent>
+                            {/* skills */}
                             <TabsContent value="skills">
-                                skills info
+                                <div className='text-center xl:text-left'>
+                                    <h3 className='h3 mb-8'>Tools I Use Everyday</h3>
+                                    <div>
+                                        <h4 className='text-xl font-semibold mb-2'>Skills </h4>
+                                        <div className='border-b border-border mb-4'></div>
+                                        {/* skills list */}
+                                        <div>
+                                            {getData(skillData, 'skills').data.map((item, index) => {
+                                                const {name} = item;
+                                                return (
+                                                    <div className='w-2/4 text-center xl:text-left mx-auto xl:mx-0' key={index}>
+                                                        <div>{name}</div> #2:19:20
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
                             </TabsContent>
                         </div>
                     </Tabs>
--- components/ui/tabs.jsx
+++ components/ui/tabs.jsx
@@ -11,7 +11,9 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2773410448.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2048524411.
+      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1.5 text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=actuve]:bg-primary data-[state=active]:text-white data-[state=active]:shadow-sm h-[48x]",
       className
     )}
     {...props} />
@@ -22,7 +24,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1.5 text-base font medium ring-offset-background transition-all focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-white data-[state=active]-sm h-[48px]",
       className
     )}
     {...props} />
